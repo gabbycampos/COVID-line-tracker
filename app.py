fc1c772623bafc20a18032671362e219c4654436
Gabriela Campos <gabriela@Gabrielas-MacBook-Pro-2.local>
2021-05-18 14:08:35 -0700

    postgres os

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ API_BASE_URL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext'
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABSE_URL', 'postgres:///covid_lt')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABSE_URL', "postgres:///covid_lt")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 app.config["SQLALCHEMY_ECHO"] = True 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'covidsecretapp')
